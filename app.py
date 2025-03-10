cc4dc4bc962a386a665256551f64630e2ccd0d78
lsy9874205 <lsy9874205@gmail.com>
2025-03-10 14:38:37 -0600

    Update app.py

--- app.py
+++ app.py
@@ -15,8 +15,18 @@ from tqdm.asyncio import tqdm_asyncio
 import asyncio
 from tqdm.asyncio import tqdm
 
+# GLOBAL SCOPE - ENTIRE APPLICATION HAS ACCESS TO VALUES SET IN THIS SCOPE #
+# ---- ENV VARIABLES ---- # 
+"""
+This function will load our environment file (.env) if it is present.
+
+NOTE: Make sure that .env is in your .gitignore file - it is by default, but please ensure it remains there.
+"""
 load_dotenv()
 
+"""
+We will load our environment variables here.
+"""
 HF_LLM_ENDPOINT = os.getenv("HF_LLM_ENDPOINT")
 HF_EMBED_ENDPOINT = os.getenv("HF_EMBED_ENDPOINT")
 HF_TOKEN = os.getenv("HF_TOKEN")
@@ -24,6 +34,15 @@ HF_TOKEN = os.getenv("HF_TOKEN")
 if not all([HF_LLM_ENDPOINT, HF_EMBED_ENDPOINT, HF_TOKEN]):
     raise ValueError("Missing required environment variables. Please check your .env file.")
 
+# ---- GLOBAL DECLARATIONS ---- #
+
+# -- RETRIEVAL -- #
+"""
+1. Load Documents from Text File
+2. Split Documents into Chunks
+3. Load HuggingFace Embeddings (remember to use the URL we set above)
+4. Index Files if they do not exist, otherwise load the vectorstore
+"""
 document_loader = TextLoader("./data/paul_graham_essays.txt")
 documents = document_loader.load()
 
@@ -88,6 +107,11 @@ async def run():
 
 hf_retriever = asyncio.run(run())
 
+# -- AUGMENTED -- #
+"""
+1. Define a String Template
+2. Create a Prompt Template from the String Template
+"""
 RAG_PROMPT_TEMPLATE = """\
 <|start_header_id|>system<|end_header_id|>
 You are a helpful assistant. You answer user questions based on provided context. If you can't answer the question with the provided context, say you don't know.<|eot_id|>
@@ -104,6 +128,10 @@ Context:
 
 rag_prompt = PromptTemplate.from_template(RAG_PROMPT_TEMPLATE)
 
+# -- GENERATION -- #
+"""
+1. Create a HuggingFaceEndpoint for the LLM
+"""
 hf_llm = HuggingFaceEndpoint(
     endpoint_url=HF_LLM_ENDPOINT,
     max_new_tokens=512,
@@ -116,6 +144,11 @@ hf_llm = HuggingFaceEndpoint(
 
 @cl.author_rename
 def rename(original_author: str):
+    """
+    This function can be used to rename the 'author' of a message. 
+
+    In this case, we're overriding the 'Assistant' author to be 'Paul Graham Essay Bot'.
+    """
     rename_dict = {
         "Assistant" : "Paul Graham Essay Bot"
     }
@@ -123,6 +156,13 @@ def rename(original_author: str):
 
 @cl.on_chat_start
 async def start_chat():
+    """
+    This function will be called at the start of every user session. 
+
+    We will build our LCEL RAG chain here, and store it in the user session. 
+
+    The user session is a dictionary that is unique to each user session, and is stored in the memory of the server.
+    """
 
     lcel_rag_chain = (
         {"context": itemgetter("query") | hf_retriever, "query": itemgetter("query")}
@@ -133,6 +173,13 @@ async def start_chat():
 
 @cl.on_message  
 async def main(message: cl.Message):
+    """
+    This function will be called every time a message is recieved from a session.
+
+    We will use the LCEL RAG chain to generate a response to the user query.
+
+    The LCEL RAG chain is stored in the user session, and is unique to each user session - this is why we can access it here.
+    """
     lcel_rag_chain = cl.user_session.get("lcel_rag_chain")
 
     msg = cl.Message(content="")
